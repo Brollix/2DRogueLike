03fff955b3e28567ea8b48e0bc032e83de924f8e
rasner3 <132791111+rasner3@users.noreply.github.com>
2023-05-20 16:22:25 -0300

    collision added

--- Collision.h
+++ Collision.h
@@ -7,27 +7,28 @@
 class Collision
 {
 public:
-	
+
 
 	void checkCollisions(Player player, Enemy enemy, Enemy texture) {
-		Vector2f enemypos= enemy.getPos();
+		Vector2f enemypos = enemy.getPos();
 		Vector2f playerpos = player.getPosition();
 		Vector2u enemysize = texture.getSize();
 		int enemyheight = enemysize.y;
 		int enemywidht = enemysize.x;
 		int height = player.height.y;
 		int width = player.width.x;
-			
+
 
 		if (playerpos.x < enemypos.x + enemywidht &&
 			playerpos.x + width > enemypos.x &&
 			playerpos.y < enemypos.y + enemyheight &&
 			playerpos.y + height > enemypos.y) {
 
-			cout << "crash" >> endl; }
+			cout << "crash" >> endl;
+		}
 
 	};
-	
-	
+
+
 };
 
